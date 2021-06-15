9834612c3b8dd85adfe8d2018a65694dbbb93610
xNeryo <benjamin.estot@gmail.com>
2021-06-16 00:05:30 +0200

    Text added

--- home/Accueil.js
+++ home/Accueil.js
@@ -1,14 +1,13 @@
 const titreSpans = document.querySelectorAll('h1 span');
-const l1 = document.querySelector('.l1');
-const l2 = document.querySelector('.l2');
+const titreSpans2 = document.querySelectorAll('h2 span');
 
 window.addEventListener('load', () => {
 
     const TL = gsap.timeline({paused: true});
 
     TL.staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.5)
-    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
-    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+    TL.staggerFrom(titreSpans2, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.5)
+
     
     TL.play();
 
